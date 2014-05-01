5ae042aef760de3ec4aa6bc9e4179b5b6309079c
Larry Gilbert <larry@l2g.to>
2014-05-01 15:29:17 -0700

    Fix class identification in spec for R18n::Locales::Th

--- r18n-core/spec/locales/th_spec.rb
+++ r18n-core/spec/locales/th_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
-describe R18n::Locales::Ru do
+describe R18n::Locales::Th do
   it "uses Thai calendar" do
     th = R18n::I18n.new('th')
     th.l(Time.at(0).utc, '%Y %y').should ==  '2513 13'
