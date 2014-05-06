884161ebd22c94df77485417f08b2d517115bd08
Larry Gilbert <larry@l2g.to>
2014-05-06 12:47:02 -0700

    Add list formatting for Galician (gl)

---
+++ r18n-core/spec/locales/gl_spec.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe R18n::Locales::Gl do
+  subject { R18n.locale('gl') }
+
+  context "formats lists in Galician" do
+    specify { subject.format_list(%w(A B)    ).should eq 'A e B'       }
+    specify { subject.format_list(%w(A B C)  ).should eq 'A, B e C'    }
+    specify { subject.format_list(%w(A B C D)).should eq 'A, B, C e D' }
+  end
+end
