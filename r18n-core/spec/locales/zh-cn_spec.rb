444c455da76ee8c90a215e33215bf0053bbc1197
Larry Gilbert <larry@l2g.to>
2014-05-10 10:02:28 -0700

    Copy Chinese locale specs to make new zh-cn and zh-tw specs

---
+++ r18n-core/spec/locales/zh-cn_spec.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe R18n::Locales::ZhCn do
+  subject { R18n.locale('zh-cn') }
+
+  context "formats lists in Chinese" do
+    specify { subject.format_list(%w(Ａ Ｂ)      ).should eq 'Ａ和Ｂ'         }
+    specify { subject.format_list(%w(Ａ Ｂ Ｃ)   ).should eq 'Ａ、Ｂ和Ｃ'     }
+    specify { subject.format_list(%w(Ａ Ｂ Ｃ Ｄ)).should eq 'Ａ、Ｂ、Ｃ和Ｄ' }
+  end
+end
